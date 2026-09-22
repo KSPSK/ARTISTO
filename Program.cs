53a96d2093255037a73f9c1dc86e30d8193600c5
Adamm0212 <adomas.pimpe@gmail.com>
2026-09-22 20:29:33 +0300

    Added Swagger documentation and example HTTP requests

--- Program.cs
+++ Program.cs
@@ -3,8 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
